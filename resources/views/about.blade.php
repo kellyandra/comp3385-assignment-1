a81003da02ad2ae778c31f6348f0f8316d7f652f
kellyandra <ms.kchristian86@gmail.com>
2024-02-13 00:14:56 -0400

    image and style added

---
+++ resources/views/about.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.app')
+@section('content')
+    <h2>Kelly Christian</h2>
+    <img src="{{ url('/images/me.jpg') }}" alt="Person on computer during the rain" />
+    <p> I'm p♥ssionate about all things technology! 
+        I love diving into new software, designing creative projects, 
+        and exploring the digital world. In my free time, you can find me 
+        indulging in hobbies like gaming, music, baking, and lending a helping hand wherever I can. 
+        The rainy weather—it's my favorite! 
+        I'm fascinated by the power of water and storms, and I hold a deep respect for the vastness of the sea."
+    </p>
+@endsection
